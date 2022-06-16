0f7c64442a61801e17af835401afcc764182a33e
Sergey <sergoopsmoskal@Gmail.com>
2022-06-16 23:05:05 +0300

    X2: Adapt for Flaming (CLO)

--- flamingo_X2.mk
+++ flamingo_X2.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/realme/X2/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from the Flamingo configuration.
+$(call inherit-product, vendor/flamingo/target/product/flamingo.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X2
+PRODUCT_NAME := flamingo_X2
 PRODUCT_DEVICE := X2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme X2
