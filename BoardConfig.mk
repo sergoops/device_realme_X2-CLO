4c4fe6ba9bb308a3006f90b74a47872a92d9cd28
Sergey <sergoopsmoskal@Gmail.com>
2022-06-16 23:05:45 +0300

    X2: Add realme specific FCM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,6 +120,7 @@ BOARD_HAVE_QCOM_FM := true
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/hidl/realme_vendor_framework_compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/framework_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += \
