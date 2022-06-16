94993674866233e0d11a8f755f36a3458f082a7e
Sergey <sergoopsmoskal@Gmail.com>
2022-06-17 00:52:15 +0300

    X2: device.mk: drop ims-ext-common
    Its already defined in Flamingo vendor makefiles

--- device.mk
+++ device.mk
@@ -233,11 +233,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml \
     $(LOCAL_PATH)/configs/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
-# IMS
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml
-
 # IMS shim
 PRODUCT_PACKAGES += lib-imsvtshim
 
